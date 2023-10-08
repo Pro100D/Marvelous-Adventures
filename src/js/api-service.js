aa3f499969e4f800711f2636dabf2927ef93040a
Danil Takiylin <danadanil900@gmail.com>
2023-10-08 16:24:23 +0200

    feat: added header-mobile, configured axios

---
+++ src/js/api-service.js
@@ -0,0 +1,14 @@
+// import md5 from 'md5';
+import axios from 'axios';
+
+// const PUBLIC_KEY = 'c123898771c31fcefa21223333cc579a';
+// const PRIVATE_KEY = 'db4f9373b5e745ee303c58638fcdfc78b6aab1bd';
+const API_KEY = 'a5837db97d72016c81a7a776f4240db9';
+// const md = md5('1', PUBLIC_KEY, PRIVATE_KEY);
+
+axios.defaults.baseURL = `https://gateway.marvel.com/v1/public`;
+
+export const getComics = async () => {
+  const { data } = await axios.get(`/comics?ts=1&apikey=${API_KEY}`);
+  return data;
+};
