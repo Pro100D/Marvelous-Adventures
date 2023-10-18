f20fa266294fdca5fa8615af4d7d2177e72a52af
Danil Takiylin <danadanil900@gmail.com>
2023-10-18 20:04:07 +0200

    refactor: added styles for the slider on mobile

--- src/js/slider-vertical.js
+++ src/js/slider-vertical.js
@@ -8,13 +8,41 @@ new Swiper('.mySwiper', {
     clickable: true,
   },
   // autoplay: {
-  //   delay: 3000,
+  //   delay: 5000,
   // },
   breakpoints: {
     767.98: {
       direction: 'vertical',
     },
   },
-  effect: 'cube',
+  effect: 'coverflow',
+
   speed: 750,
+  on: {
+    slideChange: function () {
+      const btn = document.querySelector('.hero__btn');
+      const activeBullet = document.querySelector(
+        '.swiper-pagination-bullet-active'
+      );
+      const bullets = document.querySelector('.swiper-pagination-bullet');
+
+      switch (this.activeIndex) {
+        case 0:
+          btn.style.backgroundColor = '#34387f';
+          activeBullet.style.backgroundColor = '#34387f';
+          break;
+        case 1:
+          btn.style.backgroundColor = '#5b7f3c';
+          activeBullet.style.backgroundColor = '#5b7f3c';
+          break;
+        case 2:
+          btn.style.backgroundColor = '#600404';
+          activeBullet.style.backgroundColor = '#600404';
+          break;
+
+        default:
+          break;
+      }
+    },
+  },
 });
